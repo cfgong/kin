e0acf1441d9168b923a8ec33201b559d5ec85c47
pervezn <nidapervez2020@u.northwestern.edu>
2018-02-18 09:33:42 -0600

    Barely started js

---
+++ js/setFrequency.js
@@ -0,0 +1,9 @@
+function individual_frequency() {
+  document.getElementsByClassName('frequency-opt').style.background = "blue"
+
+}
+
+function group_frequency() {
+  document.getElementById('id').style.background = "blue"; //the one you select
+  //should make individual_frequency all greyed out
+}
