dd8f4c2bf9cb1b125dc7bf8048ab92f73b3fa341
Sam Arrants <arrants8@gmail.com>
2018-02-18 19:56:10 -0600

    holy crap this is hard

---
+++ js/syncing.js
@@ -0,0 +1,24 @@
+function fbmess(){
+    document.getElementById('facebookmessenger').style.background="#73AD21";
+  };
+
+function phone(){
+    document.getElementById('phone').style.background="#73AD21";
+  };
+
+
+function Whatsapp(){
+    document.getElementById('Whatsapp').style.background="#73AD21";
+  };
+
+function Slack(){
+    document.getElementById('Slack').style.background="#73AD21";
+  };
+
+function Wechat(){
+      document.getElementById('Wechat').style.background="#73AD21";
+    };
+
+function Email(){
+    document.getElementById('email').style.background="#73AD21";
+  };
