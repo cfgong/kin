b8f4d4a679c89a248a22469d32a29b4bf5c4d537
Crystal Gong <crystalgong123@gmail.com>
2018-02-18 21:18:58 -0600

    disable individual radio buttons when family is selected

--- js/setFrequency.js
+++ js/setFrequency.js
@@ -1,9 +1,6 @@
-function individual_frequency() {
-  document.getElementsByClassName('frequency-opt').style.background = "blue"
-
+function disableIndividual(){
+     $(".individualRadio").prop("disabled", true); // 
 }
 
-function group_frequency() {
-  document.getElementById('id').style.background = "blue"; //the one you select
-  //should make individual_frequency all greyed out
-}
+
+
