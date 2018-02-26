8dc0262130a4556b6cf7253d6a6babc0e68ec5ce
Crystal Gong <crystalgong123@gmail.com>
2018-02-26 14:24:53 -0600

    fixed login on submit, goes to next page now, deleted extra username check

--- js/login.js
+++ js/login.js
@@ -1,23 +1,12 @@
 
 
 function validatePassword(){
-  username = document.getElementById("username").value,
-  confirm_username = document.getElementById("confirm_username").value,
   password = document.getElementById("password").value,
   confirm_password = document.getElementById("confirm_password").value;
-  if(username != confirm_username){
-    alert("Usernames do not match");
-  }
-  else if(password != confirm_password){
+  if(password != confirm_password){
     alert("Passwords do not match");
   }
   else{//if they're equal
-    //alert("Success!");
-    console.log(here);
-    window.location.href='../groups.html';
-    //location.href = "www.yoursite.com"
+    location.href='groups.html';
   }
 }
-
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
