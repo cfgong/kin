7d5c0cb2243a4fb2c95cfbd7b0c76f2a0c7f81f8
Crystal Gong <crystalgong123@gmail.com>
2018-02-26 15:29:17 -0600

    reverting changes

--- js/home.js
+++ js/home.js
@@ -2,8 +2,8 @@ var people = ["Victor", "Nathan", "Kaetlyn", "Kristi", "Michelle", "Bradie", "Vi
 var images = [];
 var status_ = [];
 var days = [];
-var lastPerson = 0; 
-var max = 0; 
+var lastPerson = 0;
+var max = 0;
 
 function init(){
     //populating images list
@@ -11,35 +11,35 @@ function init(){
     var t = "img/image" + i + ".jpg";
     images.push(t);
   }
-    max = images.length; 
+    max = images.length;
     //populating status list
    for (var i = 0; i < 3; i++){
     var t = "overdue";
     status_.push(t);
-  } 
+  }
     for (var i = 3; i < people.length; i++){
     var t = "upcoming";
     status_.push(t);
-  } 
-    
+  }
+
     //populating days list
     for (var i = -3; i < people.length; i++){
     days.push(Math.abs(i));
-  } 
-    
+  }
+
     var groupTitle = document.createElement("div");
     groupTitle.setAttribute('class', "groupTitle");
     groupTitle.innerHTML = "Status Bar";
     var groupId = "group"+i;
     groupTitle.setAttribute('id', groupId);
-    
+
     document.getElementById("groups").append(groupTitle);
-    
-    lastPerson = 4; 
+
+    lastPerson = 4;
     for (var i =0; i<lastPerson; i++){
         addPerson(i);
     }
-    
+
 };
 function addPerson(i){
     var group = document.createElement("div");
@@ -48,24 +48,24 @@ function addPerson(i){
     group.setAttribute('id', groupId);
     document.getElementById("groups").append(group);
     //adding the status bar
-    
-    groupStatus= document.createElement("DIV");
+
+    groupStatus= document.createElement("span");
     //groupStatus.setAttribute("class", "groupTitle");
     groupStatus.innerHTML = status_[i];
     group.append(groupStatus);
-    
-    groupDays= document.createElement("DIV");
-    //groupStatus.setAttribute("class", "groupTitle");
+
+    groupDays= document.createElement("span");
+    groupDays.setAttribute("class", "days");
     groupDays.innerHTML = days[i]+" days";
     group.append(groupDays);
-    
-    //adding an image 
+
+    //adding an image
     img = document.createElement("img");
     img.setAttribute("src", images[i]);
     img.setAttribute("class", "proPic");
     group.append(img);
     //group.innerHTML+="<br>";
-    
+
     //group title
     groupTitle = document.createElement("DIV");
     groupTitle.setAttribute("class", "groupTitle");
@@ -73,27 +73,27 @@ function addPerson(i){
     groupTitle.setAttribute("id", groupTitleId);
     groupTitle.innerHTML = people[i];
     group.append(groupTitle);
-    
-    
-    
+
+
+
     //adding a remove person button
     var removeButton = document.createElement("BUTTON");
     removeButton.innerHTML =  "Remove";
     removeButton.onclick = function(){removeGroup(groupId, lastPerson);}
     group.appendChild(removeButton);
-    
+
     //adding a postpone button
     var removeButton = document.createElement("BUTTON");
     removeButton.innerHTML =  "Postpone";
     removeButton.onclick = function(){postponeGroup(groupId, lastPerson);}
-    group.appendChild(removeButton);  
+    group.appendChild(removeButton);
 }
 function removeGroup(elementId, i){
     element = document.getElementById(elementId);
     element.remove(elementId);
     /**
     if (lastPerson >= max){
-        lastPerson = 0; 
+        lastPerson = 0;
     }
     addPerson(lastPerson);
     lastPerson++;
