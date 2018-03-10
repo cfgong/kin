73adbf4407f20312db5b019a81d5752f68a6744a
Betsy <elizabethchou2017@u.northwestern.edu>
2018-03-10 17:55:29 -0600

    Built header for onboarding

--- js/syncing.js
+++ js/syncing.js
@@ -4,7 +4,7 @@ var clicked = [false, false, false, false, false, false];
 
 function fbmess(){
     if(clicked[0] == false){
-      document.getElementById('facebookmessenger').style="background: #4CAF50; color: white;";
+      document.getElementById('facebookmessenger').style="background:var(--medium-orange); color: white;";
       clicked[0]=true;
     }
     else{
@@ -16,7 +16,7 @@ function fbmess(){
 
 function phone(){
   if(clicked[1] == false){
-    document.getElementById('phone').style="background: #4CAF50; color: white;";
+    document.getElementById('phone').style="background:var(--medium-orange); color: white;";
     clicked[1]=true;
   }
   else{
@@ -28,7 +28,7 @@ function phone(){
 
 function Whatsapp(){
   if(clicked[2] == false){
-    document.getElementById('Whatsapp').style="background: #4CAF50; color: white;";
+    document.getElementById('Whatsapp').style="background:var(--medium-orange); color: white;";
     clicked[2]=true;
   }
   else{
@@ -39,7 +39,7 @@ function Whatsapp(){
 
 function Slack(){
   if(clicked[3] == false){
-    document.getElementById('Slack').style="background: #4CAF50; color: white;";
+    document.getElementById('Slack').style="background:var(--medium-orange); color: white;";
     clicked[3]=true;
   }
   else{
@@ -50,7 +50,7 @@ function Slack(){
 
 function Wechat(){
   if(clicked[4] == false){
-    document.getElementById('Wechat').style="background: #4CAF50; color: white;";
+    document.getElementById('Wechat').style="background:var(--medium-orange)0; color: white;";
     clicked[4]=true;
   }
   else{
@@ -61,7 +61,7 @@ function Wechat(){
 
 function Email(){
   if(clicked[5] == false){
-    document.getElementById('email').style="background: #4CAF50; color: white;";
+    document.getElementById('email').style="background:var(--medium-orange); color: white;";
     clicked[5]=true;
   }
   else{
